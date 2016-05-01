6741c9805a74e350a1244717ac7d0a3cdeae9b53
Naoya Shibahara <naoya@cps.im.dendai.ac.jp>
2016-05-01 14:31:02 +0900

    Add MapsActivity

--- app/src/main/java/jp/ac/dendai/im/cps/iwitutorial/activities/MainActivity.java
+++ app/src/main/java/jp/ac/dendai/im/cps/iwitutorial/activities/MainActivity.java
@@ -1,11 +1,12 @@
 package jp.ac.dendai.im.cps.iwitutorial.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import jp.ac.dendai.im.cps.iwitutorial.R;
-import jp.ac.dendai.im.cps.iwitutorial.fragments.SurfaceSampleFragment;
+import jp.ac.dendai.im.cps.iwitutorial.fragments.MapsActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -17,10 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        manager = getSupportFragmentManager();
-
-        manager.beginTransaction()
-                .replace(R.id.container, SurfaceSampleFragment.newInstance())
-                .commit();
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+//        manager = getSupportFragmentManager();
+//
+//        manager.beginTransaction()
+//                .replace(R.id.container, SurfaceSampleFragment.newInstance())
+//                .commit();
     }
 }
